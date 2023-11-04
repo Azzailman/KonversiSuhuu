fe825c0d6f1fd22406fe4794540b6d04646f9fa5
Azzailman <azzailmank@gmail.com>
2023-11-04 14:32:54 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/konversisuhuu/KonversiSuhuu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.konversisuhuu;
+
+/**
+ *
+ * @author azzai_6qal
+ */
+public class KonversiSuhuu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
